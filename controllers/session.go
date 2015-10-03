1a0386332a26178673b7e2e440a418983b39198f
Patricio López <lopezjuripatricio@gmail.com>
2015-10-02 23:20:39 -0300

    add find user by username

--- controllers/session.go
+++ controllers/session.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/IIC2173-2015-2-Grupo2/news-api/models"
 	"github.com/IIC2173-2015-2-Grupo2/news-api/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jmcvetta/neoism"
@@ -19,10 +20,17 @@ type SessionController struct {
 Token get token
 */
 func (n *SessionController) Token(c *gin.Context) {
-	user, _ := c.PostForm("username"), c.PostForm("password")
+	username, password := c.PostForm("username"), c.PostForm("password")
+
+	if user, err := models.FindUserByUsername(n.DB, username); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "username not found"})
+
+	} else if err := util.ValidatePass(password, user.Password); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid password"})
+	}
 
 	claims := map[string]interface{}{
-		"ID": user,
+		"ID": username,
 	}
 
 	if token, err := util.Token("", claims); err != nil {
--- models/user.go
+++ models/user.go
@@ -1,10 +1,63 @@
 package models
 
+import (
+	"errors"
+
+	"github.com/jmcvetta/neoism"
+)
+
 /*
 User model
 */
 type User struct {
 	Name     string `json:"name"`
+	Username string `json:"username"`
 	Email    string `json:"email"`
-	Password string
+	Password string `json:"password"`
+}
+
+// ---------------------------------------------------------------------------
+
+/*
+GetUser returns the user with that id
+*/
+func GetUser(db *neoism.Database, id int) (*User, error) {
+	var users []User
+	if err := db.Cypher(&neoism.CypherQuery{
+		Statement: `MATCH (user:User)
+								WHERE ID(user) = {id}
+								RETURN user.name as name, user.email as email`,
+		Parameters: neoism.Props{"id": id},
+		Result:     &users,
+	}); err != nil {
+		return nil, err
+
+	} else if len(users) == 0 {
+		return nil, errors.New("not found")
+
+	} else {
+		return &users[0], nil
+	}
+}
+
+/*
+FindUserByUsername find user
+*/
+func FindUserByUsername(db *neoism.Database, username string) (*User, error) {
+	var users []User
+	if err := db.Cypher(&neoism.CypherQuery{
+		Statement: `MATCH (user:User)
+								WHERE user.username = {username}
+								RETURN user.name as name, user.username as username, user.email as email, user.password as password`,
+		Parameters: neoism.Props{"username": username},
+		Result:     &users,
+	}); err != nil {
+		return nil, err
+
+	} else if len(users) == 0 {
+		return nil, errors.New("not found")
+
+	} else {
+		return &users[0], nil
+	}
 }
